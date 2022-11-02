9ecb594874ef211d079e5d574977f511faa168bf
Pradeep <Sooraj.Pradeep@stfc.ac.uk>
2022-11-02 11:07:23 +0000

    some tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,18 +3,20 @@
 import numpy as np
 import numpy.testing as npt
 
+import pytest
 
-def test_daily_mean_zeros():
+@pytest.mark.parametrize("test, expected", 
+[
+([[0, 0],[0, 0],[0, 0]], [0, 0]),
+([[1, 2],[3, 4],[5, 6]], [3, 4])
+]
+)
+def test_daily_mean_zeros(test, expected):
     """Test that mean function works for an array of zeros."""
     from inflammation.models import daily_mean
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+        # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_mean(test), expected)
 
 
 def test_daily_mean_integers():
@@ -29,27 +31,29 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-def test_daily_min():
+@pytest.mark.parametrize("test, expected", 
+[
+([[0, 0],[0, 0],[0, 0]], [0, 0]),
+([[1, 2],[3, 4],[5, 6]], [1, 2])
+]
+)
+def test_daily_min(test, expected):
     """bla bla doc string """
     from inflammation.models import daily_min
 
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([1, 2])
-
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
+    npt.assert_array_equal(daily_min(test), expected)
 
-
-def test_daily_max():
+@pytest.mark.parametrize("test, expected", 
+[
+([[0, 0],[0, 0],[0, 0]], [0, 0]),
+([[1, 2],[3, 4],[5, 6]], [5, 6])
+]
+)
+def test_daily_max(test, expected):
     """bla bla doc string """
     from inflammation.models import daily_max
 
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([5, 6])
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    npt.assert_array_equal(daily_max(test), expected)
