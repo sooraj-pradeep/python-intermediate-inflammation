83342769b79c023da420b28d5ec84106e651e8d4
Pradeep <Sooraj.Pradeep@stfc.ac.uk>
2022-11-02 12:27:06 +0000

    debug tester

--- tests/test_models.py
+++ tests/test_models.py
@@ -53,7 +53,17 @@ def test_daily_min(test, expected):
 def test_daily_max(test, expected):
     """bla bla doc string """
     from inflammation.models import daily_max
-
-
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_max(test), expected)
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ]
+)
+def test_patient_normalise(test,expected):
+    '''ble'''
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(np.array(test)),np.array(expected), decimal=2)
